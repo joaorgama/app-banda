639e95b43b6bb0e89c745adbb13d9252425b9cae
joaorgama <joaorgama@gmail.com>
2026-02-01 22:42:57 +0000

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,6 @@
+# Ficheiro vazio ou com imports
+from .seatable_conn import get_base, add_presenca, safe_delete_presenca
+from .helpers import hash_password, converter_data_robusta, formatar_data_pt
 
+__all__ = ['get_base', 'add_presenca', 'safe_delete_presenca', 
+           'hash_password', 'converter_data_robusta', 'formatar_data_pt']
