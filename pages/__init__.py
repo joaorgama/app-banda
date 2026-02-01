2af94150a47436cadaf721c859265dd64a8c94cb
joaorgama <joaorgama@gmail.com>
2026-02-01 22:47:20 +0000

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,5 +1,2 @@
 # Este ficheiro pode estar vazio, mas é obrigatório
 # Ou pode importar os módulos:
-from . import musico, professor, maestro, direcao
-
-__all__ = ['musico', 'professor', 'maestro', 'direcao']
