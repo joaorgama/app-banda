25701e6f377049f4860735d821d7f712060e3e7e
joaorgama <joaorgama@gmail.com>
2026-02-01 22:54:42 +0000

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,6 @@
+"""
+Funções auxiliares para a aplicação BMO
+"""
 import hashlib
 from datetime import datetime
 import pandas as pd
@@ -9,21 +12,47 @@ def hash_password(password):
     return hashlib.sha256(str(password).encode()).hexdigest()
 
 def converter_data_robusta(valor):
-    """Converte vários formatos de data para date object"""
-    if not valor or str(valor) in ['None', 'nan', '']:
+    """
+    Converte vários formatos de data para date object
+    Aceita: datetime, string ISO, string PT, timestamps
+    """
+    if not valor or str(valor) in ['None', 'nan', '', 'NaT']:
         return None
+    
     if isinstance(valor, (datetime, pd.Timestamp)):
         return valor.date()
     
     str_data = str(valor).strip()
-    for fmt in ('%Y-%m-%d', '%d/%m/%Y', '%Y-%m-%d %H:%M:%S'):
+    
+    # Tentar vários formatos
+    formatos = [
+        '%Y-%m-%d',           # 2026-02-01
+        '%d/%m/%Y',           # 01/02/2026
+        '%Y-%m-%d %H:%M:%S',  # 2026-02-01 14:30:00
+        '%d-%m-%Y',           # 01-02-2026
+    ]
+    
+    for fmt in formatos:
         try:
-            return datetime.strptime(str_data.split(' ')[0].split('T')[0], fmt).date()
+            data_limpa = str_data.split(' ')[0].split('T')[0]
+            return datetime.strptime(data_limpa, fmt).date()
         except:
             continue
+    
     return None
 
 def formatar_data_pt(valor):
-    """Formata data para DD/MM/YYYY"""
+    """Formata data para formato português DD/MM/YYYY"""
     dt = converter_data_robusta(valor)
     return dt.strftime('%d/%m/%Y') if dt else "---"
+
+def validar_email(email):
+    """Validação básica de email"""
+    import re
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(pattern, str(email)) is not None
+
+def validar_telefone(telefone):
+    """Validação básica de telefone português"""
+    tel = str(telefone).replace(' ', '').replace('-', '')
+    return len(tel) == 9 and tel.isdigit()
