912b5a441afca450b4fc05a11a5893d24a58534d
joaorgama <joaorgama@gmail.com>
2026-03-12 11:33:55 +0000

    Update musico.py

--- pages/musico.py
+++ pages/musico.py
@@ -323,7 +323,7 @@ def _render_calendario_ensaios(base, ensaios, faltas, user):
 # ============================================
 
 def render(base, user):
-    from app import (
+    from cache import (
         get_musicos_cached,
         get_eventos_cached,
         get_presencas_cached,
